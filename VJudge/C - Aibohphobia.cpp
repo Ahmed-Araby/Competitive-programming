8354ec263ad9c01a79f9a4c62c25eafc06b1eb31
Ahmed Araby <ahmedaraby605@gmail.com>
2021-04-03 10:10:25 +0200

    Update C - Aibohphobia.cpp

--- VJudge/C - Aibohphobia.cpp
+++ VJudge/C - Aibohphobia.cpp
@@ -36,3 +36,56 @@ int main()
     }
     return 0;
 }
+
+
+//////////////////////////////////////////////// --- Tabular solution --- ////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+int get(int **dp, int n, int m, int i, int j)
+{
+    if(i<0 || i >=n)
+        return 0;
+    else if(j<0 || j>=m)
+        return 0;
+    return dp[i][j];
+}
+int solve(string &s, int **dp)
+{
+    // tabular form
+    int n = s.size();
+    for(int i=n-1;  i>=0; i--){
+        for(int j=0; j<n; j++){
+            if(i>=j)
+                dp[i][j] = 0;
+            else{
+                if(s[i]==s[j]){
+                    dp[i][j] = get(dp, n, n, i+1, j-1);
+                    continue;
+                }
+                int ans = 1e8;
+                ans = get(dp, n, n,i+1, j) + 1;
+                ans = min(ans, get(dp, n, n, i, j-1) + 1);
+                dp[i][j] = (ans == 1e8?0:ans);
+            }
+        }
+    }
+    return dp[0][n-1];
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int **dp = new int*[s.size()+1];
+        for(int i=0; i<=s.size(); i++){
+            dp[i] = new int [s.size()+1];
+        }
+        int res = solve(s, dp);
+        cout<<res<< endl;
+    }
+    return 0;
+}
