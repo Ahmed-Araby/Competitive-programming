7c6e6c94d3b3ecd4bf1c8c703321f7564f91a251
Ahmed Araby <ahmedaraby605@gmail.com>
2021-04-29 05:39:12 +0200

    Update O - Matching.cpp

--- atCoder/DP/bitmasks/O - Matching.cpp
+++ atCoder/DP/bitmasks/O - Matching.cpp
@@ -64,3 +64,67 @@ int main()
     cout<<count_pairing(n, comp)<< endl;
     return 0;
 }
+
+
+////////////////////////////////////////////////////////////////////////////
+/*
+    tabular solution will get TLE as this sol 
+    will build all the table including the cases that will never happen.
+*/
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+const int mod = 1e9+7;
+
+int add_mod(ll a, ll b)
+{
+    a = a%mod;
+    b = b%mod;
+    return (a+b) % mod;
+}
+
+int count_pairing(int n, bool comp[][22])
+{
+    int **dp = new int *[n+2];
+    for(int i=0; i<=n; i++)
+        dp[i] = new int [1<<n]; // pow(2, 21) -1 = 21 BIT each = 1
+
+    for(int i=n; i>=0; i--)
+    {
+        for(int j = (1<<n) -1; j>=0; j--)
+        {
+            if(i==n )
+                dp[i][j] = 1;
+            else if(j==(1<<n) - 1) // cant happen in real life.
+                dp[i][j] = -1;
+            else
+            {
+                dp[i][j] = 0;
+                for(int k=0; k<n; k++)
+                {
+                    if(!comp[i][k] || (j & (1<<k)))
+                        continue;
+                    dp[i][j] = add_mod(dp[i][j], dp[i+1][j|(1<<k)]);
+                }
+            }
+        }
+    }
+    return dp[0][0];
+}
+
+int main()
+{
+
+    int n;
+    cin>>n;
+
+    bool comp[22][22];
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            cin>>comp[i][j];
+
+    cout<<count_pairing(n, comp)<< endl;
+    return 0;
+}
