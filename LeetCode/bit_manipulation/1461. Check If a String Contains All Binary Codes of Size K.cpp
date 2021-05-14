bd3cb198ff3688a18110539958bd43869cb28e3e
Ahmed Araby <ahmedaraby605@gmail.com>
2021-05-14 19:38:36 +0200

    Update 1461. Check If a String Contains All Binary Codes of Size K.cpp

--- LeetCode/bit_manipulation/1461. Check If a String Contains All Binary Codes of Size K.cpp
+++ LeetCode/bit_manipulation/1461. Check If a String Contains All Binary Codes of Size K.cpp
@@ -47,3 +47,46 @@ public:
         return cnt == need;
     }
 };
+
+
+
+// using rolling hash technique.
+
+class Solution {
+public:
+    
+    int count_subStrs(string &s, bool *vis,  int k)
+    { // rolling hash
+        int n = s.size();
+        int cnt  = 0, need = pow(2, k);
+        int hash = 0;
+        
+        for(int i=0; i<n && cnt < need; i++){
+            
+            int bit = s[i]-'0';
+            hash = (hash << 1) & (need-1);
+            hash |= bit;
+            
+            if(i >= k-1){
+                if(vis[hash] == 0){
+                    cnt +=1;
+                    vis[hash] = 1;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+    
+    bool hasAllCodes(string s, int k) {
+        
+        int need = pow(2, k);
+        bool *vis= new bool[need+1];
+        for(int i=0; i<=need; i++)
+            vis[i] = 0;
+        
+        
+        int cnt = count_subStrs(s, vis, k);
+        return cnt == need;
+    }
+};
