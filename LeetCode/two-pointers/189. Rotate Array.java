5035872091f3d30927d2df55431ee7739ccdde90
Ahmed Araby <ahmedaraby605@gmail.com>
2022-09-15 23:33:26 +0200

    Update 189. Rotate Array.java

--- LeetCode/two-pointers/189. Rotate Array.java
+++ LeetCode/two-pointers/189. Rotate Array.java
@@ -17,3 +17,29 @@ class Solution {
             nums[i] = auxArr[i];
     }
 }
+
+///////////////////////////////////////////////////////////////////
+
+// very smart solution https://leetcode.com/problems/rotate-array/discuss/2579709/Java-or-2-Different-Approaches-or-Easy-or-Understandable-Code
+class Solution {
+    public void rotate(int[] nums, int k) {
+        // solution #2
+        // in place rotation solution 
+        // O(1) memory complexity
+        // O(k) + O(N) time complexity
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length -1);
+        reverse(nums, 0, nums.length - 1);
+    }
+    
+    public void reverse(int[] nums, int l, int r){
+        while(l < r){
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+}
