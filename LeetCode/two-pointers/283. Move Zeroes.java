61bae4f7bf94b05cdea7c137160d71d60ac81b1f
Ahmed Araby <ahmedaraby605@gmail.com>
2022-09-17 20:40:49 +0200

    Update 283. Move Zeroes.java

--- LeetCode/two-pointers/283. Move Zeroes.java
+++ LeetCode/two-pointers/283. Move Zeroes.java
@@ -1,3 +1,41 @@
+/**
+my approach to the problem
+
+we need to find the amount of positions we need to shift every cell to the left
+the amount of shift left positions we need to do for cell at pos x
+equals to the number of zeros before any cell x, 
+
+as a starting solution and we can do this using a aux array to keep number of 
+shift left steps we need to apply for each cell
+
+
+1 0 2 3 4 5
+
+1 2 3 4 5 0 -> our out put array  
+
+O(N)
+O(N)  
+number of writes on the original array is optimal cuz we are doing 1 swap for each non zero element that has at least one zero before it.
+
+
+1 0 0 0 2 4
+1 2 0 0 0 4
+1 2 4 0 0 0 -> output
+
+
+to enhance the solution in terms of space complexity, we can get rid of the 
+auxalary array.
+
+by using 2 pointers approach
+
+slow and fast
+slow one will keep track of the most left cell with zero in it as this is the cell we need to swap non zero elements with it.
+
+    | 0 
+1 2 0 2 3 4 
+all elements between the slow and fast pointer will be zeros not including the fast pointer
+*/
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int zpp = -1;
